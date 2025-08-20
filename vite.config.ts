969f6517e7ad9eefcb215bfe9bd3e2426ac461fb
Sergio Hernández Lara <sergiohernandezlara07@gmail.com>
2025-08-20 18:20:31 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+// vite.config.ts
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path, { dirname } from "path";
@@ -7,7 +8,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 export default defineConfig({
-  base: './', // ← ¡ESTA es la línea mágica!
+  base: "./",
   plugins: [react()],
   resolve: {
     alias: {
@@ -15,14 +16,19 @@ export default defineConfig({
       "@shared": path.resolve(__dirname, "shared"),
     },
   },
-  root: path.resolve(__dirname, "client"), // Directorio raíz dentro de client
+  root: path.resolve(__dirname, "client"),
   build: {
-    outDir: path.resolve(__dirname, "client", "dist"), // Directorio de salida correcto
+    outDir: path.resolve(__dirname, "client", "dist"),
     emptyOutDir: true,
-    assetsDir: "assets", // Carpeta para recursos estáticos
+    assetsDir: "assets",
     rollupOptions: {
       external: [],
     },
+  },
+  preview: {
+    host: true, // 0.0.0.0
+    allowedHosts: ["soulhealing-06jy.onrender.com"], // tu dominio de Render
+    strictPort: true,
   },
   optimizeDeps: {
     include: ["embla-carousel-react"],
